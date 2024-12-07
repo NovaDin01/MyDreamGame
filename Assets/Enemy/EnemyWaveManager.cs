524001692d898b11bb2b891b8823f3ede52f42d4
NovaDin <Danila214bulgakov@yandex.ru>
2024-12-07 20:08:54 +0300

    EnemyWave

--- Assets/Enemy/EnemyWaveManager.cs
+++ Assets/Enemy/EnemyWaveManager.cs
@@ -10,31 +10,55 @@ public class EnemyWaveManager : MonoBehaviour
     [SerializeField] private GameObject[] enemies;
     private int enemyTypeIndex;
     private int spawnPointsIndex;
-    private int waweNumber;
+    private int waveNumber;
+    private int enemyCount;
+    
+    private bool isCheckWave = true;
+    
+    [SerializeField] float waveTime;
+    private float currentWaveTime;
+        
+    private void Start()
+    {   
+        waveNumber = 1;
+        enemyCount = 5;
+        currentWaveTime = 0;
+    }
 
     private void Update()
     {
-        SpawnEnemy();
+        currentWaveTime += Time.deltaTime;
+        WaveManager();
     }
 
-    void Wave()
+    void WaveManager()
     {
-        
-    }
-
-    void SpawnEnemy()
-    {
-        StartCoroutine(TimeBetweenSpawn());
+        if (GameObject.FindGameObjectWithTag("Enemy") == null && isCheckWave)
+        {
+            isCheckWave = false; // Начинаем новую волну
+            if (currentWaveTime >= waveTime)
+            {
+                waveNumber++;
+                enemyCount += 5; // Увеличиваем количество врагов
+                Debug.Log($"WaveNumber - {waveNumber}");
+                Debug.Log($"enemyCount - {enemyCount}");
+                StartCoroutine(TimeBetweenSpawn());
+            }
+        }
     }
 
     private IEnumerator TimeBetweenSpawn()
     {
-        for (int i = 0; i < waweNumber; i++)
+        for (int i = 0; i < enemyCount; i++)
         {
-            yield return new WaitForSeconds(Random.Range(2, 5));  // Задержка
+            yield return new WaitForSeconds(Random.Range(2, 5)); // Задержка между спавнами
             enemyTypeIndex = Random.Range(0, enemies.Length);
             spawnPointsIndex = Random.Range(0, spawnPoints.Count);
             Instantiate(enemies[enemyTypeIndex], spawnPoints[spawnPointsIndex].transform.position, Quaternion.identity);
         }
-    } 
+
+        // После завершения спавна, разрешаем проверку новой волны
+        isCheckWave = true;
+        currentWaveTime = 0; // Сбрасываем таймер
+    }
 }
